689d20865cc262f4a7f4b3776eb74ee79b0ab73d
Nguyen Duc Huy <huyndhe176876@fpt.edu.vn>
2025-05-18 23:07:54 +0700

    Login bằng username/password = FE, mongoDB

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { getTheme } from './theme/theme';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import HomePage from './pages/HomePage';
-import FindBoat from './pages/FindBoat';
-import Login from './auth/Login';
-import Register from './auth/Register';
-import AboutUs from './layout/componentsFooter/AboutUs';
-import TermsAndConditions from './layout/componentsFooter/TermsAndConditions';
-import PrivacyPolicy from './layout/componentsFooter/PrivacyPolicy';
-import UserGuide from './layout/componentsFooter/UserGuide';
-import PaymentMethods from './layout/componentsFooter/PaymentMethods';
-import ContactSection from './layout/componentsFooter/ContactSection';
-import Enterprise from './components/Enterprise/Enterprise';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { getTheme } from "./theme/theme";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import HomePage from "./pages/HomePage";
+import FindBoat from "./pages/FindBoat";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import AboutUs from "./layout/componentsFooter/AboutUs";
+import TermsAndConditions from "./layout/componentsFooter/TermsAndConditions";
+import PrivacyPolicy from "./layout/componentsFooter/PrivacyPolicy";
+import UserGuide from "./layout/componentsFooter/UserGuide";
+import PaymentMethods from "./layout/componentsFooter/PaymentMethods";
+import ContactSection from "./layout/componentsFooter/ContactSection";
+import Enterprise from "./components/Enterprise/Enterprise";
 
 function App() {
-  const [mode, setMode] = useState(localStorage.getItem('themeMode') || 'light');
+  const [mode, setMode] = useState(localStorage.getItem("themeMode") || "light");
 
   const toggleTheme = () => {
     setMode((prevMode) => {
-      const newMode = prevMode === 'light' ? 'dark' : 'light';
-      localStorage.setItem('themeMode', newMode);
+      const newMode = prevMode === "light" ? "dark" : "light";
+      localStorage.setItem("themeMode", newMode);
       return newMode;
     });
   };
@@ -30,30 +30,27 @@ function App() {
   const theme = getTheme(mode);
 
   return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <Header toggleTheme={toggleTheme} mode={mode} />
-          <Routes>
-            <Route
-              path="/"
-              element={<HomePage />}
-            />
-            <Route path="/find-boat" element={<FindBoat />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/ve-chung-toi" element={<AboutUs />} />
-            <Route path="/dieu-khoan-va-dieu-kien" element={<TermsAndConditions />} />
-            <Route path="/chinh-sach-rieng-tu" element={<PrivacyPolicy />} />
-            <Route path="/huong-dan-su-dung" element={<UserGuide />} />
-            <Route path="/hinh-thuc-thanh-toan" element={<PaymentMethods />} />
-            <Route path="/lien-he-tu-van" element={<ContactSection />} />
-            <Route path="/doanh-nghiep" element={<Enterprise />} />
-            {/* Add thêm nếu có thêm */}
-          </Routes>
-          <Footer />
-        </Router>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Header toggleTheme={toggleTheme} mode={mode} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/find-boat" element={<FindBoat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/ve-chung-toi" element={<AboutUs />} />
+          <Route path="/dieu-khoan-va-dieu-kien" element={<TermsAndConditions />} />
+          <Route path="/chinh-sach-rieng-tu" element={<PrivacyPolicy />} />
+          <Route path="/huong-dan-su-dung" element={<UserGuide />} />
+          <Route path="/hinh-thuc-thanh-toan" element={<PaymentMethods />} />
+          <Route path="/lien-he-tu-van" element={<ContactSection />} />
+          <Route path="/doanh-nghiep" element={<Enterprise />} />
+          {/* Add thêm nếu có thêm */}
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
 
