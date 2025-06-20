12c03fe4c811c4d922363e60c1eadcf7cb611bea
PhamTuan2003 <phamthanhtuan09122003@gmail.com>
2025-06-20 22:54:14 +0700

    fix fe company

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'nprogress/nprogress.css';
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -5,6 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App";
 import "./index.css";
 import { persistor, store } from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,3 +22,6 @@ root.render(
 
   </Provider>
 );
+
+
+reportWebVitals();
